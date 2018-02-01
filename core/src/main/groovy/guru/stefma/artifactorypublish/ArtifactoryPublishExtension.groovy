e4da6c0c618e396a550a1af30d7f3e3f32c63ed5
StefMa <StefMaDev@outlook.com>
2018-02-01 16:21:42 +0100

    Use novoda's bintray-release as dependency
    Improved documentation

--- core/src/main/groovy/guru/stefma/artifactorypublish/ArtifactoryPublishExtension.groovy
+++ core/src/main/groovy/guru/stefma/artifactorypublish/ArtifactoryPublishExtension.groovy
@@ -1,8 +1,14 @@
 package guru.stefma.artifactorypublish
 
-import guru.stefma.androidartifacts.ArtifactsExtension
+import com.novoda.gradle.release.PublishExtension
 
-class ArtifactoryPublishExtension extends ArtifactsExtension {
+/**
+ * A ArtifactoryExtension which extends from PublishExtension to make sure that we can use one closure (our own)
+ * to use it for a bintray & a artifactory upload!
+ *
+ * You should use the `copyPropertiesTo` method to keep the PublishExtension from the BintrayRelease plugin in sync.
+ */
+class ArtifactoryPublishExtension extends PublishExtension {
 
     String artifactoryUrl = ""
 
@@ -12,6 +18,37 @@ class ArtifactoryPublishExtension extends ArtifactsExtension {
 
     String artifactoryKey
 
-    String[] publications
+    /**
+     * Copy properties from *this* to the given PublishExtension.
+     *
+     * Should be always in sync with updates. So we can make sure that the  `artifactoryPublish` extension can
+     * be used for the `bintrayUpload` as well...
+     *
+     * @param publishExtension the extension to setup. Should come from the com.novoda.binray-release plugin.
+     */
+    void copyPropertiesTo(PublishExtension publishExtension) {
+        // TODO: Split into required properties and optional one
+        publishExtension.with {
+            it.publishVersion = publishVersion
+            it.dryRun = dryRun
+            it.bintrayKey = bintrayKey
+            it.bintrayUser = bintrayUser
+            it.artifactId = artifactId
+            it.autoPublish = autoPublish
+            it.desc = desc
+            it.description = description
+            it.groupId = groupId
+            it.issueTracker = issueTracker
+            it.licences = licences
+            it.publications = publications
+            it.repoName = repoName
+            it.version = version
+            it.repository = repository
+            it.uploadName = uploadName
+            it.userOrg = userOrg
+            it.versionAttributes = versionAttributes
+            it.website = website
+        }
+    }
 
 }
--- core/src/main/groovy/guru/stefma/artifactorypublish/ArtifactoryPublishPlugin.groovy
+++ core/src/main/groovy/guru/stefma/artifactorypublish/ArtifactoryPublishPlugin.groovy
@@ -1,28 +1,38 @@
 package guru.stefma.artifactorypublish
 
-import guru.stefma.androidartifacts.AndroidArtifactsPlugin
-import guru.stefma.androidartifacts.ArtifactsExtension
+import com.novoda.gradle.release.PublishExtension
+import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.jfrog.gradle.plugin.artifactory.ArtifactoryPlugin
 
-class ArtifactoryPublishPlugin extends AndroidArtifactsPlugin {
-
-    ArtifactoryPublishExtension mPublishExtension
+/**
+ * A plugin which will create artifacts for your android and java artifacts and automatically setup the artifactory
+ * closure (from the "original" jfrog artifactory plugin) to make it easily to upload to a artifactory.
+ */
+class ArtifactoryPublishPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        super.apply(project)
+        // Create our own extension which can be setup
+        def extension = project.extensions.create('artifactoryPublish', ArtifactoryPublishExtension.class)
 
+        // Apply bintray-release (which will create the PublishRelease extension internal)
+        project.apply([plugin: 'com.novoda.bintray-release'])
+
+        // TODO: Do we need that? Either this or project.apply isn't needed...
         new ArtifactoryPlugin().apply(project)
+
         project.afterEvaluate {
+            // Setup the PublishRelease extension based on the ArtifactoryPublish extension.
+            def publishExtension = project.extensions.findByName("publish") as PublishExtension
+            extension.copyPropertiesTo(publishExtension)
+
+            // TODO: Do we need that? Either this or new ArtifactoryPlugin isn't needed...
             project.apply([plugin: 'com.jfrog.artifactory'])
-            new ArtifactoryPublishConfiguration(mPublishExtension).configure(project)
+
+            // Configure the artifactory closure with our extension
+            new ArtifactoryPublishConfiguration(extension).configure(project)
         }
     }
 
-    @Override
-    ArtifactsExtension getArtifactsExtensions(Project project) {
-        mPublishExtension = project.extensions.create('artifactoryPublish', ArtifactoryPublishExtension)
-        return mPublishExtension
-    }
 }
