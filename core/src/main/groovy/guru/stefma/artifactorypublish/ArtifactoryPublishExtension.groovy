1f3dd0d4776075cc072ca584e84a24642f748ee5
StefMa <StefMaDev@outlook.com>
2018-02-02 15:55:14 +0100

    User InvokeHelper to copy properties

--- core/src/main/groovy/guru/stefma/artifactorypublish/ArtifactoryPublishExtension.groovy
+++ core/src/main/groovy/guru/stefma/artifactorypublish/ArtifactoryPublishExtension.groovy
@@ -1,6 +1,7 @@
 package guru.stefma.artifactorypublish
 
 import com.novoda.gradle.release.PublishExtension
+import org.codehaus.groovy.runtime.InvokerHelper
 
 /**
  * A ArtifactoryExtension which extends from PublishExtension to make sure that we can use one closure (our own)
@@ -21,34 +22,15 @@ class ArtifactoryPublishExtension extends PublishExtension {
     /**
      * Copy properties from *this* to the given PublishExtension.
      *
-     * Should be always in sync with updates. So we can make sure that the  `artifactoryPublish` extension can
-     * be used for the `bintrayUpload` as well...
+     * So we can make sure that the `artifactoryPublish` extension can be used for the `bintrayUpload` as well...
      *
      * @param publishExtension the extension to setup. Should come from the com.novoda.binray-release plugin.
      */
     void copyPropertiesTo(PublishExtension publishExtension) {
-        // TODO: Split into required properties and optional one
-        publishExtension.with {
-            publishVersion = this.publishVersion
-            dryRun = this.dryRun
-            bintrayKey = this.bintrayKey
-            bintrayUser = this.bintrayUser
-            artifactId = this.artifactId
-            autoPublish = this.autoPublish
-            desc = this.desc
-            description = this.description
-            groupId = this.groupId
-            issueTracker = this.issueTracker
-            licences = this.licences
-            publications = this.publications
-            repoName = this.repoName
-            version = this.version
-            repository = this.repository
-            uploadName = this.uploadName
-            userOrg = this.userOrg
-            versionAttributes = this.versionAttributes
-            website = this.website
-        }
+        def properties = this.properties
+        properties.remove("metaClass")
+        properties.remove("class")
+        InvokerHelper.setProperties(publishExtension, properties)
     }
 
 }
