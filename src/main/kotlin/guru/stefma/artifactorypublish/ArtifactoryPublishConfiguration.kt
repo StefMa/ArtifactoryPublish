94657c0da63be799a4f4300fe016255a1abb9489
StefMa <StefMaDev@outlook.com>
2018-08-10 14:29:30 +0200

    Add missing asterics

--- src/main/kotlin/guru/stefma/artifactorypublish/ArtifactoryPublishConfiguration.kt
+++ src/main/kotlin/guru/stefma/artifactorypublish/ArtifactoryPublishConfiguration.kt
@@ -1,6 +1,7 @@
 package guru.stefma.artifactorypublish
 
 import closureOf
+import com.sun.xml.fastinfoset.util.StringArray
 import org.gradle.api.Project
 import org.jfrog.gradle.plugin.artifactory.dsl.ArtifactoryPluginConvention
 import org.jfrog.gradle.plugin.artifactory.dsl.PublisherConfig
@@ -28,7 +29,7 @@ class ArtifactoryPublishConfiguration(
             })
         }
         (project.tasks.getByName("artifactoryPublish") as ArtifactoryTask).apply {
-            publications(extension.publications)
+            publications(*extension.publications)
             setPublishArtifacts(true)
             setPublishPom(true)
         }
